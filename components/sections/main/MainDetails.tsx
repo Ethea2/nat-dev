8a716b9b02abddbd98bd0d138f68d85c2c42966d
Ethea2 <nathandres2001@gmail.com>
2023-11-12 22:12:23 +0800

    added: main header

---
+++ components/sections/main/MainDetails.tsx
@@ -0,0 +1,29 @@
+import Reveal from "@/components/animations/Reveal";
+import Image from "next/image";
+
+const MainDetails = () => {
+  return (
+    <div className="w-full flex flex-col lg:flex-row justify-center items-center p-20">
+      <div
+        id="main-details-container"
+        className="flex flex-col p-4 text-left gap-4"
+      >
+        <div id="main-details-title" className="text-6xl font-bold">
+          <Reveal>
+            Hello, I'm Nathan!
+          </Reveal>
+        </div>
+        <div className="text-left w-full">
+          <Reveal>
+            I am an aspiring web developer hailing from Manila! I am currently
+            studying in DLSU as a Bachelor of Science in Computer Science Major
+            in Software Technology.
+          </Reveal>
+        </div>
+      </div>
+      <Image src={'/profile-picture.png'} width={1200} height={600} className="z-10"/>
+    </div>
+  );
+};
+
+export default MainDetails;
---
+++ components/sections/main/MainHeader.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Reveal from "@/components/animations/Reveal";
+import { motion, useAnimate } from "framer-motion";
+import { useEffect, useState } from "react";
+import MainDetails from "./MainDetails";
+const MainHeader = () => {
+  const [scope, animate] = useAnimate();
+
+  const [size, setSize] = useState({ columns: 0, rows: 0 });
+
+  useEffect(() => {
+    generateGridCount();
+    window.addEventListener("resize", generateGridCount);
+
+    return () => window.removeEventListener("resize", generateGridCount);
+  }, []);
+
+  const generateGridCount = () => {
+    const columns = Math.floor(document.body.clientWidth / 75);
+    const rows = Math.floor(document.body.clientHeight / 75);
+
+    setSize({
+      columns,
+      rows,
+    });
+  };
+
+  const handleMouseLeave = (e) => {
+    // @ts-ignore
+    const id = `#${e.target.id}`;
+    animate(id, { background: "rgba(129, 140, 248, 0)" }, { duration: 1.5 });
+  };
+
+  const handleMouseEnter = (e) => {
+    // @ts-ignore
+    const id = `#${e.target.id}`;
+    animate(id, { background: "rgba(129, 140, 248, 1)" }, { duration: 0.15 });
+  };
+
+  return (
+    <main className="bg-black w-full h-screen flex justify-center items-center text-white">
+      <div
+        ref={scope}
+        className="absolute grid h-screen w-full grid-cols-[repeat(auto-fit,_minmax(75px,_1fr))] grid-rows-[repeat(auto-fit,_minmax(75px,_1fr))]"
+      >
+        {[...Array(size.rows * size.columns)].map((_, i) => (
+          <div
+            key={i}
+            id={`square-${i}`}
+            onMouseLeave={handleMouseLeave}
+            onMouseEnter={handleMouseEnter}
+            className="h-full w-full border-[1px] border-neutral-900"
+          />
+        ))}
+      </div>
+      <MainDetails />
+    </main>
+  );
+};
+
+export default MainHeader;
