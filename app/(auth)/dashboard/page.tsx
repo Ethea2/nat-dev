727bc7436bbd728dbe2651796200d7c2cd557f19
Ethea2 <nathandres2001@gmail.com>
2023-12-14 23:37:30 +0400

    added: route security for dashboard

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,8 +1,22 @@
 'use client'
 
 import DataDisplay from '@/components/dashboard/DataDisplay'
+import { useAuth } from '@/hooks/useAuth'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
 
 function Dashboard() {
+    const { user } = useAuth()
+    const router = useRouter()
+    useEffect(() => {
+        if (!user) {
+            router.push('/login')
+            toast('You need to login first to access this page!', {
+                type: 'error',
+            })
+        }
+    }, [user, router])
     return (
         <div className="min-h-screen w-full">
             <DataDisplay url={'/api/exp/'} title={'Experiences'} />
