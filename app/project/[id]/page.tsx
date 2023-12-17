b3ce791c10c31910820ffee619aa6a45bb0f56f8
Ethea2 <nathandres2001@gmail.com>
2023-12-17 18:58:10 +0400

    added: wrappers back

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -44,48 +44,50 @@ async function ProjectPage({ params }: { params: { id: string } }) {
         projectsPromise,
     ])
     return (
-        <section className="w-full min-h-screen flex flex-col justify-center items-center lg:p-20 lg:gap-10">
-            <div className="w-full text-center text-4xl font-bold p-10">
-                {project.title}
-            </div>
-            <div className="relative w-full h-[15rem] lg:h-[40rem]">
-                <Image
-                    src={project.image}
-                    layout="fill"
-                    alt={project.title + ' image'}
-                    className="lg:object-contain p-2"
-                />
-            </div>
-            <div className="flex justify-center items-center gap-3 text-5xl lg:text-7xl my-2 p-10">
-                {TECHSTACKS.map((tech, index) => {
-                    if (project.techstacks.includes(tech.stack))
-                        return (
-                            <div
-                                key={index}
-                                className=" hover:scale-110 transition duration-300 ease-in hover:text-indigo-600"
-                            >
-                                {tech.icon}
-                            </div>
-                        )
-                })}
-            </div>
-            <div className="w-full lg:text-left text-justify text-xl lg:text-2xl indent-5 lg:indent-10 p-10">
-                {project.body}
-            </div>
-            <div className="w-full p-0 m-0">
-                <CardCarousel
-                    items={projects}
-                    text1="My"
-                    text2="other projects"
-                />
-            </div>
-            <Link
-                href="/"
-                className="border-2 p-4 rounded-xl text-xl font-bold hover:scale-110 transition duration-300 ease-in hover:text-indigo-500 hover:border-indigo-500"
-            >
-                Go back to main page!
-            </Link>
-        </section>
+        <PageWrapper>
+            <section className="w-full min-h-screen flex flex-col justify-center items-center lg:p-20 lg:gap-10">
+                <div className="w-full text-center text-4xl font-bold p-10">
+                    {project.title}
+                </div>
+                <div className="relative w-full h-[15rem] lg:h-[40rem]">
+                    <Image
+                        src={project.image}
+                        layout="fill"
+                        alt={project.title + ' image'}
+                        className="lg:object-contain p-2"
+                    />
+                </div>
+                <div className="flex justify-center items-center gap-3 text-5xl lg:text-7xl my-2 p-10">
+                    {TECHSTACKS.map((tech, index) => {
+                        if (project.techstacks.includes(tech.stack))
+                            return (
+                                <div
+                                    key={index}
+                                    className=" hover:scale-110 transition duration-300 ease-in hover:text-indigo-600"
+                                >
+                                    {tech.icon}
+                                </div>
+                            )
+                    })}
+                </div>
+                <div className="w-full lg:text-left text-justify text-xl lg:text-2xl indent-5 lg:indent-10 p-10">
+                    {project.body}
+                </div>
+                <div className="w-full p-0 m-0">
+                    <CardCarousel
+                        items={projects}
+                        text1="My"
+                        text2="other projects"
+                    />
+                </div>
+                <Link
+                    href="/"
+                    className="border-2 p-4 rounded-xl text-xl font-bold hover:scale-110 transition duration-300 ease-in hover:text-indigo-500 hover:border-indigo-500"
+                >
+                    Go back to main page!
+                </Link>
+            </section>
+        </PageWrapper>
     )
 }
 
